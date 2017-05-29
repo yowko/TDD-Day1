ebfa4cf858a4c62db060763488b881eb46dfddf3
yowkotsai <yowkotsai@gmail.com>
2017-05-29 22:32:55 +0800

    round 2

--- TDD-Day1/GroupSum.cs
+++ TDD-Day1/GroupSum.cs
@@ -1,80 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TDD_Day1
 {
-    public interface IGroupSum
+    public class GroupSum<T>
     {
-        List<int> SumData(int n, string col);
-    }
+        private IEnumerable<T> _products;
 
-    public class GroupSum : IGroupSum
-    {
-        private List<Product> _products;
-
-        public GroupSum(List<Product> products)
+        public GroupSum(IEnumerable<T> products)
         {
             this._products = products;
         }
 
         public GroupSum()
         {
-            _products = new List<Product>();
-            #region - 塞假資料 -
-            //for (int i = 1; i < 12; i++)
-            //{
-            //    this._products.Add(new Product() { Id = i, Cost = i, Revenue = 10 + i, SellPrice = 20 + i });
-            //}
-            #endregion
+            _products = new List<T>();
         }
 
-        public List<int> SumData(int n, string col)
+        public IEnumerable<int> Sum(int groupsize, Func<T, int> SumCol)
         {
-            List<int> result = new List<int>();
-            #region - 使用 queue 實作 -          
-
-            //if (this._products == null)
-            //{
-            //    throw new NullReferenceException();
-            //}
-            //else
-            //{
-            //    var props = typeof(Product).GetProperties(BindingFlags.Public | BindingFlags.Instance).FirstOrDefault(x => x.Name == col);
-            //    var queue = new Queue<Product>(this._products);
-            //    for (int i = 0; i < (this._products.Count / n) + 1; i++)
-            //    {
-            //        int tmpCount = 0;
-            //        for (int j = 0; j < n; j++)
-            //        {
-            //            if (queue.Count != 0)
-            //            {
-            //                var tmp = queue.Dequeue();
-            //                tmpCount += (int)props.GetValue(tmp);
-            //            }
-
-            //        }
-            //        result.Add(tmpCount);
-            //    }
-            //}
-            #endregion
-
-            #region - linq 自訂分群 -
-
-            var props = typeof(Product).GetProperties(BindingFlags.Public | BindingFlags.Instance).FirstOrDefault(x => x.Name == col);
-            for (int i = 0; i < (this._products.Count / n) + 1; i++)
+            if (_products == null)
             {
-                //result.Add(this._products.Skip(i * n).Take(n).Sum(a => a.Cost));
-
-                result.Add(this._products.Skip(i * n).Take(n).Sum(a => (int)props.GetValue(a)));
+                throw new NullReferenceException();
             }
-
-            #endregion
-
-            return result;
+            for (int i = 0; i < (this._products.Count() / groupsize) + 1; i++)
+            {
+                yield return this._products.Skip(i * groupsize).Take(groupsize).Sum(SumCol);
+            }
         }
     }
 }
--- TDD-Day1/UnitTest_GroupSum.cs
+++ TDD-Day1/UnitTest_GroupSum.cs
@@ -1,120 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using ExpectedObjects;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NSubstitute;
-using NSubstitute.ExceptionExtensions;
 
 namespace TDD_Day1
 {
     [TestClass]
     public class UnitTest_GroupSum
     {
-        static List<Product> Products;
-        //[TestInitialize]
-        [ClassInitialize]
-        public static void Init(TestContext context)
+        List<Product> Products;
+        [TestInitialize]
+        public void Init()
         {
-            Products = new List<Product>();
-            for (int i = 1; i < 12; i++)
+            Products = new List<Product>()
             {
-                Products.Add(new Product() { Id = i, Cost = i, Revenue = 10 + i, SellPrice = 20 + i });
-            }
+                new Product() { Id=1 ,Cost=1 ,Revenue=11,SellPrice=21 },
+                new Product() { Id=2 ,Cost=2 ,Revenue=12,SellPrice=22 },
+                new Product() { Id=3 ,Cost=3 ,Revenue=13,SellPrice=23 },
+                new Product() { Id=4 ,Cost=4 ,Revenue=14,SellPrice=24 },
+                new Product() { Id=5 ,Cost=5 ,Revenue=15,SellPrice=25 },
+                new Product() { Id=6 ,Cost=6 ,Revenue=16,SellPrice=26 },
+                new Product() { Id=7 ,Cost=7 ,Revenue=17,SellPrice=27 },
+                new Product() { Id=8 ,Cost=8 ,Revenue=18,SellPrice=28 },
+                new Product() { Id=9 ,Cost=9 ,Revenue=19,SellPrice=29 },
+                new Product() { Id=10,Cost=10,Revenue=20,SellPrice=30 },
+                new Product() { Id=11,Cost=11,Revenue=21,SellPrice=31 }
+            };
         }
 
-        [TestMethod]
-        public void 三筆一組取cost總合_nsub_stub()
-        {
-            //arrange
-            var target = Substitute.For<IGroupSum>();
-            var expected = new List<int>() { 6, 15, 24, 21 };
-            target.SumData(3, nameof(Product.Cost)).Returns(new List<int>() { 6, 15, 24, 21 });
-
-            //act
-            var actual = target.SumData(3, nameof(Product.Cost));
-
-            //assert
-            expected.ToExpectedObject().ShouldEqual(actual);
-            //CollectionAssert.AreEqual(expected, actual);
-        }
         [TestMethod]
         public void 三筆一組取cost總合_realclass()
         {
             //arrange
-            var target = new GroupSum(Products);
+            var target = new GroupSum<Product>(Products);
             var expected = new List<int>() { 6, 15, 24, 21 };
 
             //act
-            var actual = target.SumData(3, nameof(Product.Cost));
+            var actual = target.Sum(3, a => a.Cost).ToList();
 
             //assert
-            //expected.ToExpectedObject().ShouldEqual(actual);
-            CollectionAssert.AreEqual(expected, actual);
+            expected.ToExpectedObject().ShouldEqual(actual);
         }
 
-        [TestMethod]
-        public void 四筆一組取revenue總合_nsub_stub()
-        {
-            //arrange
-            var target = Substitute.For<IGroupSum>();
-            var expected = new List<int>() { 50, 66, 60 };
-            target.SumData(4, nameof(Product.Revenue)).Returns(new List<int>() { 50, 66, 60 });
 
-            //act
-            var actual = target.SumData(4, nameof(Product.Revenue));
-
-            //assert
-            //expected.ToExpectedObject().ShouldEqual(actual);
-            CollectionAssert.AreEqual(expected, actual);
-        }
         [TestMethod]
         public void 四筆一組取revenue總合_realclass()
         {
             //arrange
-            var target = new GroupSum(Products);
+            var target = new GroupSum<Product>(Products);
             var expected = new List<int>() { 50, 66, 60 };
 
             //act
-            var actual = target.SumData(4, nameof(Product.Revenue));
+            var actual = target.Sum(4, a => a.Revenue).ToList();
 
             //assert
             expected.ToExpectedObject().ShouldEqual(actual);
-            //CollectionAssert.AreEqual(expected, actual);
         }
-        [TestMethod]
-        public void ThrowNullReferenceException_nsub_stub()
-        {
 
-            //arrange
-            var target = Substitute.For<IGroupSum>();
-            target.SumData(4, nameof(Product.Revenue)).ReturnsForAnyArgs(a => throw new NullReferenceException());
-
-            //act
-            Action action = () => target.SumData(4, nameof(Product.Revenue));
-
-
-            //assert
-            //target.Invoking(a => a.SumData(4, nameof(Product.Revenue))).ShouldThrow<NullReferenceException>();
-            action.ShouldThrow<NullReferenceException>();
-
-        }
         [TestMethod]
         public void ThrowNullReferenceException_realclass()
         {
 
             //arrange
-            var target = new GroupSum(null);
+            var target = new GroupSum<Product>(null);
 
             //act
-            //Action action = () => target.SumData(4, nameof(Product.Revenue));
+            Action action = () => target.Sum(4, a => a.Revenue).ToList();
 
 
             //assert
-            //action.ShouldThrow<NullReferenceException>();
-            target.Invoking(a => a.SumData(4, nameof(Product.Revenue))).ShouldThrow<NullReferenceException>();
-
-
+            action.ShouldThrow<NullReferenceException>();
         }
         [TestMethod]
         [ExpectedException(typeof(NullReferenceException))]
@@ -122,10 +79,10 @@ namespace TDD_Day1
         {
 
             //arrange
-            var target = new GroupSum(null);
+            var target = new GroupSum<Product>(null);
 
             //act
-            var actual = target.SumData(4, nameof(Product.Revenue));
+            var actual = target.Sum(4, a => a.Revenue).ToList();
 
 
             //assert - Expects exception
